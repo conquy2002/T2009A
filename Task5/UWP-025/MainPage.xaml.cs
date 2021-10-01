2a63ce57ea4025acda023df85cfe3a837834dc3d
conquy2002 <72489231+conquy2002@users.noreply.github.com>
2021-10-01 16:19:28 +0700

    New

---
+++ Task5/UWP-025/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP_025
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MyCheckBox_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Checkbox.Text = MyCheckBox.IsChecked.ToString();
+        }
+
+        private void YesRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButtonCheckBox.Text = (bool)YesRadioButton.IsChecked ? "Yes" : "No";
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBoxText == null) return;
+
+            var combo = (ComboBox)sender;
+            var item = (ComboBoxItem)combo.SelectedItem;
+            ComboBoxText.Text = item.Content.ToString();
+        }
+
+        private void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var SelectItems = MyListBox.Items.Cast<ListBoxItem>()
+                                .Where(p => p.IsSelected)
+                                .Select(t => t.Content.ToString()).ToArray();
+            ListBoxText.Text = string.Join(", ", SelectItems);
+        }
+
+        private void MyToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleButtonText.Text = MyToggleButton.IsChecked.ToString();
+        }
+    }
+}
