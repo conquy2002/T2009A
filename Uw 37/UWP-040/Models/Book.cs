f8954f95c5ca5fdf95b5c6240ade56fc340a7200
conquy2002 <72489231+conquy2002@users.noreply.github.com>
2021-10-06 13:10:21 +0700

    new

---
+++ Uw 37/UWP-040/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_040.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string CoverImg { get; set; }
+    }
+    public class BookManager
+    {
+        public static  List<Book> GetBooks()
+        {
+            var books = new List<Book>();
+
+            books.Add(new Book { BookId = 1, Title = "a", Author = "b", CoverImg = "Assets/StoreLogo.png" });
+            books.Add(new Book { BookId = 2, Title = "a", Author = "b", CoverImg = "Assets/StoreLogo.png" });
+            books.Add(new Book { BookId = 3, Title = "a", Author = "b", CoverImg = "Assets/StoreLogo.png" });
+            return books;
+        }
+    }
+}
