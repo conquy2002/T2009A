86cc100e23140f162b536c0097100c0f626c9342
conquy2002 <72489231+conquy2002@users.noreply.github.com>
2021-12-31 16:36:46 +0700

    new

---
+++ Humman Resources Management/Humman Resources Management/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Humman_Resources_Management.Models
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Position { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
